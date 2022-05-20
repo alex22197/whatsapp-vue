310368f49aeea97c4d7e67b1513f8bbf0901f2f6
Alejandro García <alejandrogarciaolmeda@gmail.com>
2022-05-20 09:54:22 +0200

    First commit

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+const props = defineProps({
+  msg: String,
+  user: String,
+  createdAt: Number,
+});
+const state = reactive({ date: new Date(props.createdAt).toLocaleString() });
+</script>
+<template>
+  <div class="msg">
+    {{ state.date }} - <span v-if="props.user">{{ props.user }}</span>
+    <span v-else>Anónimo</span> : {{ props.msg }}
+  </div>
+</template>
+<style>
+.msg {
+  background: lightblue;
+  display: inline-block;
+  padding: 1em;
+  border-radius: 20px;
+  margin: 1em;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+import { useUserStore } from "@/stores/user";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const state = reactive({ user: "" });
+const { setUser } = useUserStore();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setUser(state.user);
+  router.push({ path: "/chat", replace: true });
+};
+</script>
+<template>
+  <form action="">
+    <label for="name">Tu nombre:</label
+    ><input type="text" name="user" v-model="state.user" />
+    <button @click="handleSubmit">Entrar</button>
+  </form>
+</template>
+
+<style></style>
